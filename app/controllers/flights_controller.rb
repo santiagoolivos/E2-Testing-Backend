76fbd2f8c314328b237781df9653168f613eda98
Raimundo Loeser <raimundo.loeser@uc.cl>
2022-06-07 11:36:56 -0400

    Feat: adapt controllers

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,16 +1,19 @@
 class FlightsController < ApplicationController
-  before_action :set_flight, only: %i[ show update destroy ]
+  before_action :set_flight, only: %i[show update destroy]
 
   # GET /flights
   def index
-    @flights = Flight.all
-
-    render json: @flights
+    @flights = []
+    Flight.all.each do |flight|
+      @flights << flight.as_json(include: %i[arrival departure seats])
+    end
+    render json: @flights, status: :ok
   end
 
   # GET /flights/1
   def show
-    render json: @flight
+    render json: { error: 'No se ha encontrado el vuelo' }, status: :not_found if @flight.blank?
+    render json: @flight.as_json(include: %i[arrival departure seats]), status: :ok if @flight.present?
   end
 
   # POST /flights
@@ -18,14 +21,27 @@ class FlightsController < ApplicationController
     @flight = Flight.new(flight_params)
 
     if @flight.save
-      render json: @flight, status: :created, location: @flight
+      create_seats(@flight)
+      render json: @flight.as_json(include: %i[arrival departure seats]), status: :created, location: @flight
     else
       render json: @flight.errors, status: :unprocessable_entity
     end
   end
 
+  def create_seats(flight)
+    10.times do |row|
+      Seat.create(flight_id: flight.id, used: false, seat_code: "A#{row + 1}")
+      Seat.create(flight_id: flight.id, used: false, seat_code: "B#{row + 1}")
+      Seat.create(flight_id: flight.id, used: false, seat_code: "C#{row + 1}")
+      Seat.create(flight_id: flight.id, used: false, seat_code: "D#{row + 1}")
+    end
+  end
+
   # PATCH/PUT /flights/1
   def update
+    render json: { error: 'No se ha encontrado el vuelo' }, status: :not_found if @flight.blank?
+    return unless @flight.present?
+
     if @flight.update(flight_params)
       render json: @flight
     else
@@ -35,17 +51,23 @@ class FlightsController < ApplicationController
 
   # DELETE /flights/1
   def destroy
+    render json: { error: 'No se ha encontrado el vuelo' }, status: :not_found if @flight.blank?
+    return unless @flight.present?
+
     @flight.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_flight
-      @flight = Flight.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def flight_params
-      params.require(:flight).permit(:code, :date, :arrival_id, :departure_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_flight
+    @flight = Flight.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    @flight = nil
+  end
+
+  # Only allow a list of trusted parameters through.
+  def flight_params
+    params.require(:flight).permit(:code, :date, :arrival_id, :departure_id)
+  end
 end
