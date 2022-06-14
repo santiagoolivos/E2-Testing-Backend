9f3492dab17f532087e7a576ebe040d96a0471ae
tomasalvarez13 <tomasalvarez13@github.com>
2022-06-14 15:08:29 -0400

    0 errores

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,7 +4,7 @@ require "database_cleaner/active_record"
 
 describe "GET '/' - homepage title", :type => :feature do
     it 'welcomes the user to React App' do
-        visit('http://localhost:3001/')
+        visit('https://main--magnificent-queijadas-851f64.netlify.app/')
         expect(page.title).to have_content("React App")
         puts 'the homepage title is React App'
     end
@@ -12,7 +12,7 @@ end
 
 describe "Registrarse", :js => true, :type => :feature do
     it 'registrarse' do
-        visit('http://localhost:3001/')
+        visit('https://main--magnificent-queijadas-851f64.netlify.app/')
         click_on 'Registrarse'
         fill_in 'Nombre', with: 'Batman'
         fill_in 'Apellido', with: 'Alvarez'
@@ -27,19 +27,19 @@ end
 
 describe "Log In", :js => true, :type => :feature do
     it 'registrarse' do
-        visit('http://localhost:3001/')
+        visit('https://main--magnificent-queijadas-851f64.netlify.app/')
         fill_in 'Email', with: 'Batman2@gotica.com'
         fill_in 'Contraseña', with: '1234'
         click_on 'Iniciar sesión'
         # page.save_screenshot
-        expect(page).to have_content("Reservas")
+        expect(page).to have_content("Mis Vuelos")
         puts 'Logeado'
     end
 end
 
 describe "Happy Path Buscar Vuelo", :js => true, :type => :feature do
     it 'buscar vuelo' do
-        visit('http://localhost:3001/')
+        visit('https://main--magnificent-queijadas-851f64.netlify.app/')
         fill_in 'Email', with: 'Batman2@gotica.com'
         fill_in 'Contraseña', with: '1234'
         click_on 'Iniciar sesión'
@@ -54,7 +54,7 @@ end
 
 describe "Happy Path Reservar Vuelo", :js => true, :type => :feature do
     it 'buscar vuelo' do
-        visit('http://localhost:3001/')
+        visit('https://main--magnificent-queijadas-851f64.netlify.app/')
         fill_in 'Email', with: 'Batman2@gotica.com'
         fill_in 'Contraseña', with: '1234'
         click_on 'Iniciar sesión'
@@ -64,10 +64,10 @@ describe "Happy Path Reservar Vuelo", :js => true, :type => :feature do
         click_on 'Buscar'
         click_on 'Seleccionar asientos'
         print("Si este test falla es posible que sea debido a que el asiento ya está seleccionado. Por favor intentar con un asiento disponible.")
-        find('#A3').click
+        find('#D3').click
         fill_in 'nombre_pasajero', with: 'Tomás'
         click_on 'Agregar Asientos'
-        expect(page).to have_content("A3")
+        expect(page).to have_content("Tomás")
     end
 end
   
